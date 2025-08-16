5306934640c3d871ec92a5eaa967875a3bd8d18c
Binit Poddar <binitpoddar006@gmail.com>
2025-08-16 16:26:54 +0530

    backend file

---
+++ Back-end/gambling_data/src/main/java/com/example/gambling_data/GamblingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.gambling_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamblingDataApplication.class, args);
+	}
+
+}
---
+++ Back-end/gambling_data/src/test/java/com/example/gambling_data/GamblingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gambling_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
